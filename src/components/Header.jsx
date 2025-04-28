c0ce3762fee0edc131afd06d86dc0e9ac881229a
Natallia Sukharava <natallia.sukharava@student.ehu.lt>
2025-04-29 01:06:03 +0300

    homework-3: connected API, cart counter, links in the footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
-import './Header.css';
+import { useContext } from "react";
+import { CartContext } from "../CartContext";
+import "./Header.css";
 
 function Header() {
+  const { cartCount, cartTotal } = useContext(CartContext);
+
   return (
     <header className="header">
       <div className="header-container">
-        <div>
-          <img
-            src="public/logo.svg"
-            alt="logo"
-            className="header-logo"
-          />
+        <div className="logo-wrapper">
+          <img src="/logo.svg" alt="logo" className="header-logo" />
         </div>
 
         <nav className="header-nav">
@@ -17,14 +17,14 @@ function Header() {
           <span className="nav-item active">Menu</span>
           <span className="nav-item">Company</span>
           <span className="nav-item login">Login</span>
-
-          <div className="cart">
-            <img
-              src="public/cart.svg"
-              alt="basket"
-              className="cart-icon"
-            />
-            <span className="cart-badge">0</span>
+          <div className="cart-wrapper">
+            <div className="cart">
+              <img src="/cart.svg" alt="basket" className="cart-icon" />
+              <span className="cart-badge">{cartCount}</span>
+            </div>
+            <div className="cart-total">
+              Total: ${cartTotal.toFixed(2)}
+            </div>
           </div>
         </nav>
       </div>
